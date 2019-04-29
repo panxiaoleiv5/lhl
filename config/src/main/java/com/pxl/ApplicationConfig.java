441582a272049a004409e8e56090f8e5dadb4e3a
panxiaoleiv5 <1244240143@qq.com>
2019-04-29 16:45:25 +0800

    config

---
+++ config/src/main/java/com/pxl/ApplicationConfig.java
@@ -0,0 +1,18 @@
+package com.pxl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author pxl
+ * @description
+ * @date 2019/4/29 16:42
+ */
+@SpringCloudApplication
+@EnableConfigServer
+public class ApplicationConfig {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationConfig.class, args);
+    }
+}
